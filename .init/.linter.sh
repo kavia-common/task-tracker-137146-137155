4bc4b17c95632e9bcd02f244636b18409d29b844
Kavia AI <production@kavia.ai>
2025-08-03 19:23:41 +0000

    CheckPoint - cgb4023242

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/task-tracker-137146-137155/todo_frontend
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
